3410ee9841968eb61aaa8c586dc95dbdf7cca043
qirat hussain <engineering4qirat@gmail.com>
2026-07-01 10:16:06 +0500

    check age

--- ticket-machine.py
+++ ticket-machine.py
@@ -4,3 +4,9 @@ if hieght<= 120:
     print("sorry,you have to grow taller to ride.")
 else: 
     print("you can take the ride.")
+    age= int(input("What is your age?"))
+    if age > 18:
+        bill= 10
+    else: 
+        bill= 7
+    
