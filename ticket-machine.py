1abbf6402b4b6cdb6c5e88471b4462be0bde5c8f
qirat hussain <engineering4qirat@gmail.com>
2026-07-01 11:53:15 +0500

    forbit yournger than 4 and older than 70

--- ticket-machine.py
+++ ticket-machine.py
@@ -1,14 +1,17 @@
 print("Welcome to the ticket machine!")
+bill=0
 hieght= float(input("what is your hieght?  cm"))
+age=int(input("What is your age?"))
 if hieght<= 120:
     print("sorry,you have to grow taller to ride.")
+elif age <=4 or age> 70:
+    print("Sorry,your age is not suitable for the ride.")
 else: 
     print("you can take the ride.")
-    age= int(input("What is your age?"))
-    if age > 18:
+    if age > 18 or age<55:
         bill= 10
-    else: 
-        bill= 7
+    else:
+        bill=7
     photo= int(input('''
     Do you want photography?
     0. no
