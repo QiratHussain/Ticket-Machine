8c0fe9cd100c55b4e50bc29254032c607ad22720
qirat hussain <engineering4qirat@gmail.com>
2026-07-01 10:14:08 +0500

    initial commit

---
+++ ticket-machine.py
@@ -0,0 +1,7 @@
+print("Welcome to the ticket machine!")
+hieght= float(input("what is your hieght?  cm"))
+if hieght<= 120:
+    print("sorry,you have to grow taller to ride.")
+else: 
+    print("you can take the ride.")
+    
